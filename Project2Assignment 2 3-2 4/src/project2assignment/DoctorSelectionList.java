9cd6eb9c051eebdda376f25f6d10311495047c77
Eva Moubarak <emoubarak@hotmail.com>
2014-10-27 21:03:52 +1100

    Fixed GUI of Doctor selection list
    Made it consistent with other forms

--- Project2Assignment 2 3-2 4/src/project2assignment/DoctorSelectionList.java
+++ Project2Assignment 2 3-2 4/src/project2assignment/DoctorSelectionList.java
@@ -65,6 +65,7 @@ public class DoctorSelectionList extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Welcome = new javax.swing.JLabel();
         jInternalFrame1 = new javax.swing.JInternalFrame();
         jLabel1 = new javax.swing.JLabel();
         searchField = new javax.swing.JTextField();
@@ -73,13 +74,38 @@ public class DoctorSelectionList extends javax.swing.JFrame {
         doctorTable = new javax.swing.JTable();
         selectButton = new javax.swing.JButton();
         cancelButton = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        patientButton = new javax.swing.JButton();
+        patientLabel = new javax.swing.JLabel();
+        patientTab = new javax.swing.JLabel();
+        billingButton = new javax.swing.JButton();
+        billingLabel = new javax.swing.JLabel();
+        billingTab = new javax.swing.JLabel();
+        messagesButton = new javax.swing.JButton();
+        messagesLabel = new javax.swing.JLabel();
+        messagesTab = new javax.swing.JLabel();
+        logo = new javax.swing.JLabel();
+        scheduleButton = new javax.swing.JButton();
+        scheduleLabel = new javax.swing.JLabel();
+        scheduleTab = new javax.swing.JLabel();
+        Welcome1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        Welcome.setBackground(new java.awt.Color(0, 0, 0));
+        Welcome.setForeground(new java.awt.Color(255, 255, 255));
+        Welcome.setText("Welcome, ");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jInternalFrame1.setTitle("Browse Doctor");
         jInternalFrame1.setVisible(true);
+        jInternalFrame1.getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setText("Find doctor");
+        jLabel1.setFont(new java.awt.Font("Georgia", 0, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Find doctor:");
+        jInternalFrame1.getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(840, 80, 90, 40));
+        jInternalFrame1.getContentPane().add(searchField, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 88, 181, 30));
 
         searchButton.setText("Search");
         searchButton.addActionListener(new java.awt.event.ActionListener() {
@@ -87,6 +113,7 @@ public class DoctorSelectionList extends javax.swing.JFrame {
                 searchButtonActionPerformed(evt);
             }
         });
+        jInternalFrame1.getContentPane().add(searchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(1100, 90, -1, -1));
 
         doctorTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -101,12 +128,15 @@ public class DoctorSelectionList extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(doctorTable);
 
+        jInternalFrame1.getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 120, 1010, 430));
+
         selectButton.setText("Select");
         selectButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 selectButtonActionPerformed(evt);
             }
         });
+        jInternalFrame1.getContentPane().add(selectButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(1090, 560, 100, 40));
 
         cancelButton.setText("Cancel");
         cancelButton.addActionListener(new java.awt.event.ActionListener() {
@@ -114,63 +144,122 @@ public class DoctorSelectionList extends javax.swing.JFrame {
                 cancelButtonActionPerformed(evt);
             }
         });
+        jInternalFrame1.getContentPane().add(cancelButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(980, 560, 100, 40));
 
-        javax.swing.GroupLayout jInternalFrame1Layout = new javax.swing.GroupLayout(jInternalFrame1.getContentPane());
-        jInternalFrame1.getContentPane().setLayout(jInternalFrame1Layout);
-        jInternalFrame1Layout.setHorizontalGroup(
-            jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jInternalFrame1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jInternalFrame1Layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(cancelButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(selectButton, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(12, 12, 12)))
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jInternalFrame1Layout.createSequentialGroup()
-                .addContainerGap(256, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(searchButton)
-                .addGap(247, 247, 247))
-        );
+        jLabel3.setFont(new java.awt.Font("Georgia", 0, 60)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(234, 234, 234));
+        jLabel3.setText("Genetech");
+        jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel3MouseClicked(evt);
+            }
+        });
+        jInternalFrame1.getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 10, 250, 90));
 
-        jInternalFrame1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {cancelButton, selectButton});
+        patientButton.setBorderPainted(false);
+        patientButton.setOpaque(false);
+        patientButton.setContentAreaFilled(false);
+        patientButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                patientButtonMouseClicked(evt);
+            }
+        });
+        patientButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                patientButtonActionPerformed(evt);
+            }
+        });
+        jInternalFrame1.getContentPane().add(patientButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 200, 170, 80));
 
-        jInternalFrame1Layout.setVerticalGroup(
-            jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jInternalFrame1Layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addGroup(jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(searchButton))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 294, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(selectButton)
-                    .addComponent(cancelButton))
-                .addContainerGap(31, Short.MAX_VALUE))
-        );
+        patientLabel.setFont(new java.awt.Font("Georgia", 0, 18)); // NOI18N
+        patientLabel.setForeground(new java.awt.Color(255, 255, 255));
+        patientLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/users.png"))); // NOI18N
+        patientLabel.setText("Patient");
+        jInternalFrame1.getContentPane().add(patientLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 210, -1, -1));
+
+        patientTab.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/button2.png"))); // NOI18N
+        jInternalFrame1.getContentPane().add(patientTab, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 200, -1, -1));
+
+        billingButton.setBorderPainted(false);
+        billingButton.setOpaque(false);
+        billingButton.setContentAreaFilled(false);
+        billingButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                billingButtonMouseClicked(evt);
+            }
+        });
+        jInternalFrame1.getContentPane().add(billingButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 280, 170, 80));
+
+        billingLabel.setFont(new java.awt.Font("Georgia", 0, 18)); // NOI18N
+        billingLabel.setForeground(new java.awt.Color(255, 255, 255));
+        billingLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/credit_card.png"))); // NOI18N
+        billingLabel.setText("Billing");
+        jInternalFrame1.getContentPane().add(billingLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 290, -1, -1));
+
+        billingTab.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/button2.png"))); // NOI18N
+        jInternalFrame1.getContentPane().add(billingTab, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 280, -1, -1));
+
+        messagesButton.setBorderPainted(false);
+        messagesButton.setOpaque(false);
+        messagesButton.setContentAreaFilled(false);
+        messagesButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                messagesButtonMouseClicked(evt);
+            }
+        });
+        jInternalFrame1.getContentPane().add(messagesButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 360, 170, 80));
+
+        messagesLabel.setFont(new java.awt.Font("Georgia", 0, 18)); // NOI18N
+        messagesLabel.setForeground(new java.awt.Color(255, 255, 255));
+        messagesLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/comments1.png"))); // NOI18N
+        messagesLabel.setText("Messages");
+        jInternalFrame1.getContentPane().add(messagesLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 370, -1, -1));
+
+        messagesTab.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/button2.png"))); // NOI18N
+        jInternalFrame1.getContentPane().add(messagesTab, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 360, -1, -1));
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/logo glow.png"))); // NOI18N
+        jInternalFrame1.getContentPane().add(logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 0, -1, -1));
+
+        scheduleButton.setBorderPainted(false);
+        scheduleButton.setOpaque(false);
+        scheduleButton.setContentAreaFilled(false);
+        scheduleButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                scheduleButtonMouseClicked(evt);
+            }
+        });
+        jInternalFrame1.getContentPane().add(scheduleButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 170, 80));
+
+        scheduleLabel.setFont(new java.awt.Font("Georgia", 0, 18)); // NOI18N
+        scheduleLabel.setForeground(new java.awt.Color(255, 255, 255));
+        scheduleLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/edit.png"))); // NOI18N
+        scheduleLabel.setText("Schedule");
+        jInternalFrame1.getContentPane().add(scheduleLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 130, -1, -1));
+
+        scheduleTab.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/button2.png"))); // NOI18N
+        jInternalFrame1.getContentPane().add(scheduleTab, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, -1, -1));
+
+        Welcome1.setBackground(new java.awt.Color(0, 0, 0));
+        Welcome1.setForeground(new java.awt.Color(255, 255, 255));
+        Welcome1.setText("Welcome, ");
+        jInternalFrame1.getContentPane().add(Welcome1, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 0, -1, 30));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/project2assignment/backdrop.png"))); // NOI18N
+        jInternalFrame1.getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(-20, -10, -1, 910));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jInternalFrame1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jInternalFrame1, javax.swing.GroupLayout.PREFERRED_SIZE, 1250, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jInternalFrame1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jInternalFrame1, javax.swing.GroupLayout.PREFERRED_SIZE, 690, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
@@ -189,6 +278,35 @@ public class DoctorSelectionList extends javax.swing.JFrame {
         toSearch = searchField.getText();
         searchDoctorTable();
     }//GEN-LAST:event_searchButtonActionPerformed
+
+    private void jLabel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseClicked
+        new HomePage().setVisible(true);
+        this.dispose();// TODO add your handling code here:
+    }//GEN-LAST:event_jLabel3MouseClicked
+
+    private void scheduleButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_scheduleButtonMouseClicked
+        new HomePage().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_scheduleButtonMouseClicked
+
+    private void patientButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_patientButtonMouseClicked
+        new PatientView().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_patientButtonMouseClicked
+
+    private void patientButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_patientButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_patientButtonActionPerformed
+
+    private void billingButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_billingButtonMouseClicked
+        new Billing().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_billingButtonMouseClicked
+
+    private void messagesButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_messagesButtonMouseClicked
+        new InstantMessaging().setVisible(true);
+        this.dispose();// TODO add your handling code here:
+    }//GEN-LAST:event_messagesButtonMouseClicked
 
     /**
      * @param args the command line arguments
@@ -226,11 +344,28 @@ public class DoctorSelectionList extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Welcome;
+    private javax.swing.JLabel Welcome1;
+    private javax.swing.JButton billingButton;
+    private javax.swing.JLabel billingLabel;
+    private javax.swing.JLabel billingTab;
     private javax.swing.JButton cancelButton;
     private javax.swing.JTable doctorTable;
     private javax.swing.JInternalFrame jInternalFrame1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel logo;
+    private javax.swing.JButton messagesButton;
+    private javax.swing.JLabel messagesLabel;
+    private javax.swing.JLabel messagesTab;
+    private javax.swing.JButton patientButton;
+    private javax.swing.JLabel patientLabel;
+    private javax.swing.JLabel patientTab;
+    private javax.swing.JButton scheduleButton;
+    private javax.swing.JLabel scheduleLabel;
+    private javax.swing.JLabel scheduleTab;
     private javax.swing.JButton searchButton;
     private javax.swing.JTextField searchField;
     private javax.swing.JButton selectButton;
