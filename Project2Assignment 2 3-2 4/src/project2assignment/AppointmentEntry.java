63be320da49653cd4ff9bc0a9aa9e81666c76171
greg-jacobson <g.jacobson@unsw.edu.au>
2014-10-31 12:35:51 +1100

    appointments are now made on the quarter hour

--- Project2Assignment 2 3-2 4/src/project2assignment/AppointmentEntry.java
+++ Project2Assignment 2 3-2 4/src/project2assignment/AppointmentEntry.java
@@ -85,7 +85,12 @@ public class AppointmentEntry extends javax.swing.JFrame {
         appointmentDayComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
         appointmentDayComboBox.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
-        appointmentMinuteComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "00", "30" }));
+        appointmentMinuteComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "00", "15", "30", "45" }));
+        appointmentMinuteComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                appointmentMinuteComboBoxActionPerformed(evt);
+            }
+        });
 
         appointmentMonthComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "10/2014", "11/2014", "12/2014", "01/2015", "02/2015", "03/2015", "04/2015", "05/2015" }));
         appointmentMonthComboBox.addActionListener(new java.awt.event.ActionListener() {
@@ -283,6 +288,10 @@ public class AppointmentEntry extends javax.swing.JFrame {
     private void createButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createButtonActionPerformed
 // TODO add your handling code here:
     }//GEN-LAST:event_createButtonActionPerformed
+
+    private void appointmentMinuteComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_appointmentMinuteComboBoxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_appointmentMinuteComboBoxActionPerformed
 
         /**
      * @param accessLevel the accessLevel to set
